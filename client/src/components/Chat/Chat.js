289b50891a78e2ea68a964f6aab9b7f61cb1ed79
vivek <vivek.koyani@gmail.com>
2020-02-26 23:02:23 +0530

    define

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,7 +7,7 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-
+    const ENDPOINT = 'localhost:5000';
     useEffect(() => {
         const { name, room} = queryString.parse(location.search);
 
