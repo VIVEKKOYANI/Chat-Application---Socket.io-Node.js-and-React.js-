0f769caa6c0923e3ef97bbb851ecf14984b569e2
vivek <vivek.koyani@gmail.com>
2020-03-01 14:16:20 +0530

    remove user

--- server/users.js
+++ server/users.js
@@ -19,6 +19,12 @@ const addUser = ({ id,name,room }) => {
 
 const removeUser = () => {
 
+    const index = users.findIndex((users) => users.id === id);
+
+    if(index !== -1){
+        return users.splice(index, 1)[0];
+    }
+
 }
 
 const getUser = () => {
