30f817a6dbece84810dd1a49c772584a2890d6f9
vivek <vivek.koyani@gmail.com>
2020-03-01 14:36:06 +0530

    addUser call

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersByRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,16 +13,12 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
-
     socket.on('join', ({name, room}, callback) => {
-        console.log(name, room);
+        const {error,user} = addUser({id: socket.id, name, room});
 
-        const error = true;
+        if(error) return callback(error);
 
-        if(error){
-            callback({ error: 'error'});
-        }
+        socket.join(user.room);
     });
 
     socket.on('disconnect', () => {
