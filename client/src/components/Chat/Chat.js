71968cf98bcebe82ee447511ba38d43fd1b0bd73
vivek <vivek.koyani@gmail.com>
2020-03-29 11:39:54 +0530

    send messages function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -37,7 +37,31 @@ const Chat = ({ location }) => {
     });
   }, [messages]);
 
-  return <h1>Chat</h1>;
+  // function for sending messages
+
+  const sendMessage = event => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
