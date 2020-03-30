6e5bdcac1af6c9170e943072fb5235d9ffefea76
vivek <vivek.koyani@gmail.com>
2020-03-30 21:36:06 +0530

    add format code and room name add

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import onlineIcon from '../../icons/onlineIcon.png'
-import closeIcon from '../../icons/closeIcon.png'
-import './InfoBar.css'
+import React from "react";
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+import "./InfoBar.css";
 
-const InfoBar = () => {
-    <div className="infoBar">
-        <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online image" />
-            <h3>roomName</h3>
-        </div>
-        <div className="rightInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close image" /></a>
-        </div>
+const InfoBar = ({ room }) => {
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online image" />
+      <h3>{room}</h3>
     </div>
-}
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img src={closeIcon} alt="close image" />
+      </a>
+    </div>
+  </div>;
+};
 
 export default InfoBar;
